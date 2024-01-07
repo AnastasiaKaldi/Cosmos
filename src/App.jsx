e30023245adb36a67e368660c2fbbab8b7a6fe8b
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-01-07 21:56:34 +0000

    Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import solar from "../public/solar.mp4";
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <video autoPlay loop muted className="video-background">
+        <source src={solar} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <p>React App</p>
     </div>
   );
