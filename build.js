fa0a4d6c92b9b28733d7f9096e5d6552c0272913
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-06-29 19:21:36 +0100

    Deployment

---
+++ build.js
@@ -0,0 +1,17 @@
+import esbuild from "esbuild";
+import sassPlugin from "esbuild-plugin-sass";
+
+esbuild
+  .build({
+    entryPoints: ["../src/main.jsx"],
+    bundle: true,
+    outfile: "../public/bundle.js",
+    minify: true,
+    sourcemap: true,
+    loader: { ".js": "jsx" },
+    plugins: [sassPlugin()],
+    define: {
+      "process.env.NODE_ENV": '"production"',
+    },
+  })
+  .catch(() => process.exit(1));
